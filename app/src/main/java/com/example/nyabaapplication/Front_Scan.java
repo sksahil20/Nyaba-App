62f9de8cf9cb2520ad834e3d843e7a8defde4376
dona Mondal <donamondal25@gmail.com>
2021-08-06 13:23:09 +0530

    modify

--- app/src/main/java/com/example/nyabaapplication/Front_Scan.java
+++ app/src/main/java/com/example/nyabaapplication/Front_Scan.java
@@ -15,6 +15,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Rational;
 import android.util.Size;
 import android.view.TextureView;
@@ -32,17 +33,18 @@ public class Front_Scan extends AppCompatActivity {
     private String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA",
             "android.permission.WRITE_EXTERNAL_STORAGE"};
     AutoFitTextureView textureView;
-    String storeFrontScan="Nyaba.document.front.jpg";
-    android.widget.Button scanAgain=findViewById(R.id.imgCapture_again);
-    ImageView scannedImg= findViewById(R.id.scannedFrontImage);
-    android.widget.Button ScanNow =findViewById(R.id.imgCapture_front);
-    android.widget.Button next=findViewById(R.id.next);
-    TextView frontText=findViewById(R.id.frontScanText);
-    ImageView backArrow=findViewById(R.id.imageView_back_arrow);
-    TextView frontScanText=findViewById(R.id.frontSide);
-    ImageView idFront=findViewById(R.id.idFront);
-    android.widget.Button startScan=findViewById(R.id.startScan);
-    TextureView scanner= findViewById(R.id.frontScanner);
+    String storeFrontScan="/Nyaba_document_front.jpg";
+    android.widget.Button scanAgain;
+    ImageView scannedImg;
+    android.widget.Button ScanNow;
+    android.widget.Button next;
+    TextView frontText;
+    ImageView backArrow;
+    TextView frontScanText;
+    ImageView idFront;
+    android.widget.Button startScan;
+    TextureView scanner;
+
 
 
     @Override
@@ -50,6 +52,7 @@ public class Front_Scan extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front_scan);
         textureView = findViewById(R.id.frontScanner);
+        views();
         onCreateLayouts();
         if(allPermissionGranted()) {
             startCamera();
@@ -73,18 +76,28 @@ public class Front_Scan extends AppCompatActivity {
     }
 
     private void startCamera() {
+        System.out.print(">>>>>>>>>>>>>>>>>>>>>>START CAMERA<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
         CameraX.unbindAll();
+        System.out.print("................................unbind..........................");
         Rational aspectRatio = new Rational(textureView.getWidth(), textureView.getHeight());
+        System.out.print("''''''''''''''''''''''''''Rational Ratio''''''''''''''''''''''''''''''");
         Size screen = new Size(textureView.getWidth(), textureView.getHeight());
+        System.out.print("''''''''''''''''''''''''''Size Screen''''''''''''''''''''''''''''''");
         PreviewConfig pConfig = new PreviewConfig.Builder().setTargetAspectRatio(aspectRatio).setTargetResolution(screen).build();
+        System.out.print("*******************************Preview config*********************************");
         Preview preview = new Preview(pConfig);
+        System.out.print("&&&&&&&&&&&&&&&&&&&&&& NEW PREVIEW &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
         preview.setOnPreviewOutputUpdateListener(
                 new Preview.OnPreviewOutputUpdateListener() {
                     @Override
                     public void onUpdated(Preview.PreviewOutput output) {
+                        System.out.print("&&&&&&&&&&&&&&&&&&&&&& onUpdate &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
                         ViewGroup parent = (ViewGroup) textureView.getParent();
+                        System.out.print("&&&&&&&&&&&&&&&&&&&&&& View Group &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
                         parent.removeView(textureView);
+                        System.out.print("&&&&&&&&&&&&&&&&&&&&&& Remove VIEW &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
                         parent.addView(textureView, 0);
+                        System.out.print("&&&&&&&&&&&&&&&&&&&&&& ADD VIEW &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
 
                         textureView.setSurfaceTexture(output.getSurfaceTexture());
                     }
@@ -92,18 +105,24 @@ public class Front_Scan extends AppCompatActivity {
         ImageCaptureConfig imageCaptureConfig = new ImageCaptureConfig.Builder().setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
                 .setTargetRotation(getWindowManager().getDefaultDisplay().getRotation()).build();
         final ImageCapture imgCap = new ImageCapture(imageCaptureConfig);
+        System.out.print("&&&&&&&&&&&&&&&&&&&&&& Image Capture &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
         findViewById(R.id.imgCapture_front).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 File file = new File(getFilesDir()+storeFrontScan);
+                System.out.print("&&&&&&&&&&&&&&&&&&&&&& File Created &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+                System.out.print("&&&&&&&&&&&&&&&&&&&&&& File Name----"+getFilesDir()+storeFrontScan);
 
                 imgCap.takePicture(file, new ImageCapture.OnImageSavedListener() {
                     @Override
                     public void onImageSaved(@NonNull File file) {
                         String msg = "Pic captured at " + file.getAbsolutePath();
+                        System.out.print("&&&&&&&&&&&&&&&&&&&&&& Captured at-----"+file.getAbsolutePath());
                         Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
                         layoutChangesAfterScanning();
+                        System.out.print("&&&&&&&&&&&&&&&&&&&&&& Layout Changed &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
                         readImgFromFile(v);
+                        System.out.print("&&&&&&&&&&&&&&&&&&&&&& read method called &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
                     }
 
 
@@ -130,17 +149,24 @@ public class Front_Scan extends AppCompatActivity {
         if(imgFile.exists())
         {
             scannedImg.setImageURI(Uri.fromFile(imgFile));
+            System.out.print("&&&&&&&&&&&&&&&&&&&&&& image read----"+getFilesDir()+storeFrontScan);
 
         }
     }
 
     void clearMyFiles() {
         File imgFile = new File(getFilesDir() + storeFrontScan);
-        if (imgFile != null) {
-            imgFile.delete();
+        Log.d("clearing: ", "start");
+        if(imgFile!=null) {
+            Log.d("clearing: ", "does exist? " + imgFile.exists());
+            Log.d("clearing: ", "does exist? " + imgFile.getAbsolutePath());
+            boolean b = imgFile.delete();
+            Log.d("clearing: ", "" + b);
+            Log.d("clearing: ", "does exist? " + imgFile.exists());
+            Log.d("clearing: ", "does exist? " + imgFile.getAbsolutePath());
+            System.out.print("+++++++++++++++++DELETE+++++++++++++++++++++++++");
         }
-    }
-
+        }
 
         public void next(View view) {
         Intent myIntent = new Intent(Front_Scan.this,back_scan.class);
@@ -159,15 +185,9 @@ public class Front_Scan extends AppCompatActivity {
 
         startScan.setVisibility(View.INVISIBLE);
         frontText.setVisibility(View.INVISIBLE);
-        backArrow.setVisibility(View.INVISIBLE);
         frontScanText.setVisibility(View.INVISIBLE);
+        idFront.setVisibility(View.INVISIBLE);
 
-    }
-
-    public void scanAgain(View view) {
-       clearMyFiles();
-       onCreateLayouts();
-       layoutAfterScanAgain();
     }
 
     public void onCreateLayouts()
@@ -176,7 +196,6 @@ public class Front_Scan extends AppCompatActivity {
         scannedImg.setVisibility(View.INVISIBLE);
         scanAgain.setVisibility(View.INVISIBLE);
         next.setVisibility(View.INVISIBLE);
-        idFront.setVisibility(View.INVISIBLE);
         ScanNow.setVisibility(View.INVISIBLE);
     }
 
@@ -193,6 +212,23 @@ public class Front_Scan extends AppCompatActivity {
        scannedImg.setVisibility(View.VISIBLE);
        ScanNow.setVisibility(View.INVISIBLE);
        next.setVisibility(View.VISIBLE);
+       idFront.setVisibility(View.INVISIBLE);
+       backArrow.setVisibility(View.VISIBLE);
+    }
+
+    public void views()
+    {
+        scanAgain=findViewById(R.id.imgCapture_again);
+        scannedImg= findViewById(R.id.scannedFrontImage);
+        ScanNow =findViewById(R.id.imgCapture_front);
+        next=findViewById(R.id.next);
+        frontText=findViewById(R.id.frontScanText);
+        backArrow=findViewById(R.id.imageView_back_arrow);
+        frontScanText=findViewById(R.id.frontSide);
+        idFront=findViewById(R.id.idFront);
+        startScan=findViewById(R.id.startScan);
+        scanner= findViewById(R.id.frontScanner);
+
     }
 
     @Override
@@ -208,4 +244,11 @@ public class Front_Scan extends AppCompatActivity {
         }
     }
 
+    public void scanAgainFront(View view) {
+        System.out.print("______________________________________DELETE___________________________________");
+        clearMyFiles();
+        System.out.print("$$$$$$$$$$$$$$$$$$$$$$$$$$$DELETE$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        onCreateLayouts();
+        layoutAfterScanAgain();
+    }
 }
